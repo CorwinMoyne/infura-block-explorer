f594144271e809446cdffdfbbdae3e03416dba28
Corwin Moyne <c_moyne@yahoo.com>
2024-02-16 17:44:50 +0000

    Add blocks loading skeleton

--- app/explorer/Blocks/BlocksLoadingSkeleton.tsx
+++ app/explorer/Blocks/BlocksLoadingSkeleton.tsx
@@ -1,5 +1,50 @@
+import { INITIAL_NUMBER_OF_BLOCKS } from "@/constants";
+
 const BlocksLoadingSkeleton = () => {
-  return <div>loading blocks...</div>;
+  return (
+    <section className="px-10 py-14 grid gap-5">
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 lg:gap-14 w-full place-items-center max-w-7xl">
+        {Array.from({ length: INITIAL_NUMBER_OF_BLOCKS }).map((_, index) => (
+          <div
+            key={index}
+            className="text-kimberly-100 bg-kimberly-700 grid gap-4 shadow-md"
+          >
+            <div className="border-b-[1px] border-kimberly-400">
+              <div className="py-4 px-3 w-full flex justify-between">
+                <div className="grid gap-1">
+                  <div className="w-[90px] h-[20px] animate-pulse">
+                    <div className="bg-gray-300 h-full w-full rounded-sm" />
+                  </div>
+                  <div
+                    className="flex space-x-3 text-sm"
+                    data-testid="block-header-timestamp"
+                  >
+                    <div className="w-[110px] h-[20px] animate-pulse">
+                      <div className="bg-gray-300 h-full w-full rounded-sm" />
+                    </div>
+                  </div>
+                </div>
+
+                <div
+                  className="flex space-x-3"
+                  data-testid="block-header-transactions"
+                >
+                  <div className="w-[50px] h-[20px] animate-pulse">
+                    <div className="bg-gray-300 h-full w-full rounded-sm" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="grid grid-cols-10 gap-[0.6rem] px-3 pb-4 min-h-[265px] content-start">
+              {Array.from({ length: 25 }).map((_, index) => (
+                <div key={index} className="w-4 h-4 bg-gray-300 animate-pulse" />
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default BlocksLoadingSkeleton;
