b0d738f428a756db67146a863ee98a0b144130f9
Corwin Moyne <c_moyne@yahoo.com>
2024-02-16 22:05:32 +0000

    Centre content

--- app/explorer/Blocks/BlocksGrid/BlocksGrid.tsx
+++ app/explorer/Blocks/BlocksGrid/BlocksGrid.tsx
@@ -2,7 +2,7 @@
 
 import { getBlocks } from "@/app/actions";
 import { IBlock } from "@/types";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Block } from "../Block";
 
 interface BlocksGridProps {
@@ -11,13 +11,31 @@ interface BlocksGridProps {
 
 /**
  * The block grid
- * 
+ *
  * @param initialBlocks The initial blocks from the server
  * @returns JSX.Element
  */
 const BlocksGrid = ({ initialBlocks }: BlocksGridProps) => {
   const [blocks, setBlocks] = useState(initialBlocks);
   const [isLoading, setIsLoading] = useState(false);
+  const [gridWidth, setGridWidth] = useState<number | undefined>();
+
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  /**
+   * Set the grid width so the load more btn is always centered under it
+   */
+  function getGridWidth() {
+    setGridWidth(ref.current?.offsetWidth);
+  }
+
+  useEffect(() => {
+    getGridWidth();
+
+    window.addEventListener("resize", getGridWidth);
+
+    return () => window.removeEventListener("resize", getGridWidth);
+  }, []);
 
   /**
    * Loads the next 12 blocks
@@ -38,6 +56,7 @@ const BlocksGrid = ({ initialBlocks }: BlocksGridProps) => {
   return (
     <section className="px-10 py-14 grid gap-5 max-h-[900px] overflow-x-auto">
       <div
+        ref={ref}
         className="blocks grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 lg:gap-14 w-full place-items-center max-w-7xl"
         data-testid="explorer-blocks"
       >
@@ -45,7 +64,7 @@ const BlocksGrid = ({ initialBlocks }: BlocksGridProps) => {
           <Block key={block.number} block={block} />
         ))}
       </div>
-      <div className="flex justify-center w-full">
+      <div style={{ width: `${gridWidth}px` }} className="flex justify-center">
         {isLoading ? (
           <div className="text-white">Loading more blocks...</div>
         ) : (
