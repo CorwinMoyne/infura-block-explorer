c3cf1888c2ddcee3f418b9664957555c56914947
Corwin Moyne <c_moyne@yahoo.com>
2024-02-25 20:54:10 +0000

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { Navbar } from "@/components";
 import StoreProvider from "@/providers/storeProvider";
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Quicksand } from "next/font/google";
 import "./globals.css";
@@ -25,6 +26,7 @@ export default function RootLayout({
           <Navbar />
           <main className="flex-grow ml-12 sm:ml-24">{children}</main>
         </StoreProvider>
+        <Analytics />
       </body>
     </html>
   );
